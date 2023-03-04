2c41537efd536b0778e106630016a9b247e70e4f
Milton David Casnanzuela <milton.casnanzuela@epn.edu.ec>
2023-03-04 16:38:27 -0500

    Añadir en app.module.ts los componentes y modificacion del app..commponent.html llamando todos los componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './componets/footer/footer.component';
+import { HeaderComponent } from './componets/header/header.component';
+import { SlideComponent } from './componets/slide/slide.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SlideComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
